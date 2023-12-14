454c463a5013753da30e20aa6e868c66f6203274
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-12-14 19:34:11 +0900

    feat 보스 레이드 기능 추가

--- src/main/java/bossmonster/controller/GameController.java
+++ src/main/java/bossmonster/controller/GameController.java
@@ -2,12 +2,15 @@ package bossmonster.controller;
 
 import bossmonster.domain.AttackInfo;
 import bossmonster.domain.BossMonster;
+import bossmonster.domain.Player;
+import bossmonster.domain.RandomDamageGenerator;
 import bossmonster.view.OutputView;
 import bossmonster.view.hanlder.InputHandler;
 
 public class GameController {
     private final InputHandler inputHandler;
     private final OutputView outputView;
+    private int count = 0;
 
     public GameController(InputHandler inputHandler, OutputView outputView) {
         this.inputHandler = inputHandler;
@@ -15,25 +18,61 @@ public class GameController {
     }
 
     public void start() {
+        BossMonster bossMonster = setUpBossMonster();
+        Player player = setUpPlayer();
+
+        outputView.printRaidStartMessage();
+        outputView.printNewLine();
+
+        while (true) {
+            outputView.printBossMonsterStatus(bossMonster);
+            if (count == 0) {
+                outputView.printInitialBossMonsterAppearance();
+            }
+            if (count > 0) {
+                outputView.printHitBossMonsterAppearance();
+            }
+            outputView.printPlayerStatus(player);
+
+            outputView.printAttackCommandInputMessage();
+            String attackCommand = inputHandler.receiveValidatedAttackCommand();
+            outputView.printNewLine();
+            AttackInfo attackInfo = AttackInfo.from(attackCommand);
+
+            int playerAttackDamage = player.attack(attackInfo);
+            outputView.printPlayerAttackMessage(playerAttackDamage);
+            bossMonster.reduceHpBy(playerAttackDamage);
+            if (bossMonster.die()) {
+                outputView.printPlayerWinningMessage(player, count);
+                break;
+            }
+
+            int bossMonsterAttackDamage = bossMonster.attack(new RandomDamageGenerator());
+            outputView.printBossMonsterAttackMessage(bossMonsterAttackDamage);
+            outputView.printNewLine();
+            player.reduceHpBy(bossMonsterAttackDamage);
+            if (player.die()) {
+                outputView.printBossMonsterWinningMessage(bossMonster, player);
+                break;
+            }
+            count++;
+        }
+    }
+
+    private BossMonster setUpBossMonster() {
         outputView.printBossMonsterHpInputMessage();
         int bossMonsterHp = inputHandler.receiveValidatedBossMonsterHp();
         outputView.printNewLine();
+        return BossMonster.from(bossMonsterHp);
+    }
 
-        BossMonster bossMonster = new BossMonster(bossMonsterHp);
-
+    private Player setUpPlayer() {
         outputView.printPlayerNameInputMessage();
         String playerName = inputHandler.receiveValidatedPlayerName();
         outputView.printNewLine();
-
-        outputView.printPlayerStatus();
+        outputView.printPlayerStatusInputMessage();
         int[] status = inputHandler.receiveValidatedPlayerStatus();
         outputView.printNewLine();
-
-        outputView.printAttackCommandInputMessage();
-        String attackCommand = inputHandler.receiveValidatedAttackCommand();
-        outputView.printNewLine();
-
-        AttackInfo attackInfo = AttackInfo.from(attackCommand);
-
+        return Player.of(playerName, status);
     }
 }
--- src/main/java/bossmonster/view/OutputView.java
+++ src/main/java/bossmonster/view/OutputView.java
@@ -1,5 +1,8 @@
 package bossmonster.view;
 
+import bossmonster.domain.BossMonster;
+import bossmonster.domain.Player;
+
 public class OutputView {
 
     public void printBossMonsterHpInputMessage() {
@@ -10,14 +13,88 @@ public class OutputView {
         System.out.println("플레이어의 이름을 입력해주세요");
     }
 
-    public void printPlayerStatus() {
+    public void printPlayerStatusInputMessage() {
         System.out.println("플레이어의 HP와 MP를 입력해주세요.(,로 구분)");
+    }
+
+    public void printRaidStartMessage() {
+        System.out.println("보스 레이드를 시작합니다!");
+    }
+
+    public void printBossMonsterStatus(BossMonster bossMonster) {
+        System.out.println("============================");
+        System.out.printf("BOSS HP [%d/%d]\n", bossMonster.getHp(), bossMonster.getInitialHp());
+    }
+
+    public void printInitialBossMonsterAppearance() {
+        System.out.println("""
+        ____________________________
+           ^-^
+         / 0 0 \\
+        (   "   )
+         \\  -  /
+          - ^ -
+        ____________________________
+        """);
+    }
+
+    public void printHitBossMonsterAppearance() {
+        System.out.println("""
+        ____________________________
+           ^-^
+         / x x \\
+        (   "\\  )
+         \\  ^  /
+          - ^ -
+        ____________________________
+        """);
+    }
+
+    public void printPlayerStatus(Player player) {
+        System.out.printf("%s HP [%d/%d] MP [%d/%d]\n", player.getName(), player.getHp(), player.getInitialHp(),
+                player.getMp(), player.getInitialMp());
+        System.out.println("============================");
+        printNewLine();
     }
 
     public void printAttackCommandInputMessage() {
         System.out.println("어떤 공격을 하시겠습니까?");
         System.out.println("1. 물리 공격");
         System.out.println("2. 마법 공격");
+    }
+
+    public void printPlayerAttackMessage(int damge) {
+        if (damge == 10) {
+            System.out.printf("물리 공격을 했습니다. (입힌 데미지: %d)\n", damge);
+        }
+        if (damge == 20) {
+            System.out.printf("마법 공격을 했습니다. (입힌 데미지: %d)\n", damge);
+        }
+    }
+
+    public void printBossMonsterAttackMessage(int damage) {
+        System.out.printf("보스가 공격 했습니다. (입힌 데미지: %d)\n", damage);
+    }
+
+    public void printPlayerWinningMessage(Player player, int count) {
+        printNewLine();
+        System.out.printf("%s 님이 %d번의 전투 끝에 보스 몬스터를 잡았습니다.\n", player.getName(), count);
+    }
+
+    public void printBossMonsterWinningMessage(BossMonster bossMonster, Player player) {
+        printBossMonsterStatus(bossMonster);
+        System.out.println("""
+        ____________________________
+           ^-^
+         / ^ ^ \\
+        (   "   )
+         \\  3  /
+          - ^ -
+        ____________________________
+        """);
+        printPlayerStatus(player);
+        System.out.printf("%s의 HP가 0이 되었습니다.\n", player.getName());
+        System.out.println("보스 레이드에 실패했습니다.");
     }
 
     public void printNewLine() {

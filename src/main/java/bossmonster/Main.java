363495476d75991f317e95a886c5b836ecfd80f5
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-12-14 19:42:11 +0900

    feat : 게임컨트롤러 실행 기능 추가

--- src/main/java/bossmonster/Main.java
+++ src/main/java/bossmonster/Main.java
@@ -1,7 +1,22 @@
 package bossmonster;
 
+import bossmonster.controller.GameController;
+import bossmonster.view.ErrorView;
+import bossmonster.view.InputView;
+import bossmonster.view.OutputView;
+import bossmonster.view.hanlder.InputHandler;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        InputHandler inputHandler = initHandler();
+        OutputView outputView = new OutputView();
+        GameController controller = new GameController(inputHandler, outputView);
+        controller.start();
+    }
+
+    private static InputHandler initHandler() {
+        InputView inputView = new InputView();
+        ErrorView errorView = new ErrorView();
+        return new InputHandler(inputView, errorView);
     }
 }
